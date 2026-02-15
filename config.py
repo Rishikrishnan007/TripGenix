ee1f6005bc561ea877871dd4a50ae0916aff90fc
S.Rishikrishnan <rishikrishnan456@gmail.com>
2026-02-15 14:48:01 +0530

    Add configuration settings for Trip Genix application

---
+++ config.py
@@ -0,0 +1,10 @@
+# Trip Genix Configuration
+
+# Database settings
+DATABASE_URL = 'your_database_url_here'
+DATABASE_USERNAME = 'your_username_here'
+DATABASE_PASSWORD = 'your_password_here'
+
+# API Keys
+API_KEY_1 = 'your_api_key_here'
+API_KEY_2 = 'your_api_key_here'
