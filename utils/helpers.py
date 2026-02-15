cf1ef12e71674fddbe851d143f7dd1ee15b5e04e
S.Rishikrishnan <rishikrishnan456@gmail.com>
2026-02-15 14:48:54 +0530

    Create utils/helpers.py with utility functions for Trip Genix

---
+++ utils/helpers.py
@@ -0,0 +1,37 @@
+# Utility Helper Functions for Trip Genix
+
+"""
+This module contains utility helper functions that can be used across the Trip Genix application.
+"""
+
+
+def format_trip_date(date):
+    """
+    Format trip date to required format.
+
+    Args:
+        date (str): Date to be formatted.
+
+    Returns:
+        str: Formatted date string.
+    """
+    # Implementation goes here
+    pass
+
+
+def calculate_trip_cost(distance, rate):
+    """
+    Calculate the total cost of a trip based on distance and rate.
+
+    Args:
+        distance (float): Distance of the trip in kilometers.
+        rate (float): Cost per kilometer.
+
+    Returns:
+        float: Total cost of the trip.
+    """
+    # Implementation goes here
+    pass
+
+
+# More utility functions can be added as needed.
